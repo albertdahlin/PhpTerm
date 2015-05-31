d14a12a20aad053beb97e8a05a692f2c40386c14
Albert Dahlin <info@albertdahlin.com>
2015-05-31 16:04:32 +0200

    Added exception class

---
+++ Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dahl\PhpTerm;
+
+/**
+ * Exception class.
+ *
+ * @copyright Copyright (C) 2015 Albert Dahlin
+ * @author Albert Dahlin <info@albertdahlin.com>
+ * @license GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
+ */
+class Exception extends \Exception
+{
+}

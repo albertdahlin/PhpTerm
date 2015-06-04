ce0492fec0a0290e994482a22d3a4cad73734903
Albert Dahlin <info@albertdahlin.com>
2015-06-05 00:15:23 +0200

    Rewrite of options element

--- Output/Options.php
+++ Output/Options.php
@@ -13,78 +13,93 @@ class Options
  extends Element
 {
     protected $_options = array();
-    protected $_selected;
+    protected $_focused;
+    protected $_width;
+    protected $_index;
 
     public function setOptions($options)
     {
-        $this->_options = $options;
-        $this->_text = array();
+        $this->_options = array();
+        $width          = 0;
+
+        foreach ($options as $key => $value) {
+            if (is_array($value)) {
+                if (isset($value['label'], $value['value'])) {
+                    $value['key']     = $key;
+                    $this->_options[] = $value;
+                }
+                $width = max($width, mb_strlen($value['label']));
+            } else {
+                $this->_options[] = array(
+                    'key'   => $key,
+                    'value' => $key,
+                    'label' => $value,
+                );
+                $width = max($width, mb_strlen($value));
+            }
+        }
+
+        $this->_width = $width;
+        $this->_text  = array();
 
         return $this;
     }
 
-    public function setSelected($key)
+    public function setFocused($key)
     {
-        $this->_selected = $key;
+        $this->_focused = $key;
 
         return $this;
     }
 
     public function getWidth()
     {
-        $width = 0;
-        foreach ($this->_options as $row) {
-            $width = max($width, mb_strlen($row));
-        }
-
-        return $width;
+        return $this->_width;
     }
 
     protected function _getIndex()
     {
         $index = 0;
-        if ($this->_selected) {
-            foreach ($this->_options as $key => $value) {
-                if ($key === $this->_selected) {
-                    return $index;
-                    break;
+        $this->_index = 0;
+        if ($this->_focused) {
+            foreach ($this->_options as $idx => $option) {
+                if ($option['key'] === $this->_focused) {
+                    $this->_index = $idx;
+                    return $idx;
                 }
-                $index++;
             }
         }
+
         return 0;
     }
 
-    public function getOption()
+    public function getSelected()
     {
-        $index = $this->_getIndex();
-        $max = count($this->_options) - 1;
-        $size = $this->getSize();
-        $startRow = $this->_getStartRow();
-        $maxHeight = $size['row'] - $startRow;
-        $start  = 0;
-        $width = $this->getWidth() + 2;
+        $index      = $this->_getIndex();
+        $max        = count($this->_options) - 1;
+        $size       = $this->getSize();
+        $startRow   = $this->_getStartRow();
+        $maxHeight  = min($size['row'] - $startRow, $this->getMaxHeight(), $max + 1);
+        $width      = $this->getWidth();
+        $start      = $this->_getStart($index, $max, $maxHeight);
+        $input      = $this->getInput();
+        $key        = $input->getKeys();
+        $strPad     = $this->_getStrPad();
 
         while (true) {
-            $this->_text = array();
+            $this->_text        = array();
             $this->_hasChanges  = true;
-            $this->_selected    = null;
-            $i = -1;
-            $j = 0;
+            $this->_focused     = null;
+            $stop               = $maxHeight + $start;
 
-            foreach ($this->_options as $value => $label) {
-                $i++;
-                if ($start > $i) {
-                    continue;
-                }
-                if (($maxHeight) <= $j++) {
-                    break;
-                }
+            for ($i = $start; $i < $stop; $i++) {
+                $option = $this->_options[$i];
+                $label = $this->_prepareLabel(
+                    str_pad($option['label'], $width, ' ', $strPad),
+                    $i
+                );
                 if ($index == $i) {
-                    $this->_selected = $value;
-                }
-                $label = str_pad(" $label ", $width, ' ', $this->_getStrPad());
-                if ($this->_selected === $value) {
+                    $this->_focused = $option['key'];
                     $this->_text[] = array(
                         'color' => 'black',
                         'background' => 'light_grey',
@@ -98,72 +113,58 @@ class Options
             parent::render();
             $this->applyFocus($index - $start);
 
-            $input = $this->getInput();
-            $key   = $input->getKeys();
-            $char  = $input->readChar(
-                'jkJKGg',
-                array(
-                    $key::ESC,
-                    $key::ENTER,
-                    $key::UP,
-                    $key::DOWN,
-                    $key::END,
-                    $key::HOME,
-                    $key::PGUP,
-                    $key::PGDOWN,
-                    $key::CTRL_B,
-                    $key::CTRL_F,
-                )
-            );
+            $char  = $input->readChar();
 
             switch ($char) {
                 case 'j':
                 case $key::DOWN:
                     $index = min($index + 1, $max);
-                    if ($start + $maxHeight <= $index) {
-                        $start++;
-                    }
                     break;
 
                 case 'k':
                 case $key::UP:
                     $index = max($index - 1, 0);
-                    if ($start > $index) {
-                        $start--;
-                    }
                     break;
 
                 case 'g':
                 case $key::HOME:
                     $index = 0;
-                    $start = 0;
                     break;
 
                 case 'G':
                 case $key::END:
                     $index = $max;
-                    $start = $max - $maxHeight + 1;
                     break;
 
                 case 'J':
                 case $key::PGDOWN:
                 case $key::CTRL_F:
                     $index = min($index + $maxHeight, $max);
-                    $start = max($index - $maxHeight + 1, 0);
                     break;
 
                 case 'K':
                 case $key::CTRL_B:
                 case $key::PGUP:
                     $index = max($index - $maxHeight, 0);
-                    $start = $index;
                     break;
 
-                case $key::ENTER:
-                    return $this->_selected;
-                case $key::ESC:
-                    return;
+                case $key::CTRL_D:
+                    $index = min($index + floor($maxHeight / 2), $max);
+                    break;
+
+                case $key::CTRL_U:
+                    $index = max($index - floor($maxHeight / 2), 0);
+                    break;
+
+                default:
+                    $return = $this->_keyEvent($char);
+
+                    if ($return !== null) {
+                        return $return;
+                    }
             }
+            $this->_index   = $index;
+            $start          = $this->_getStart($index, $max, $maxHeight, $start);
         }
     }
 
@@ -189,5 +190,59 @@ class Options
             default:
                 return STR_PAD_RIGHT;
         }
+    }
+
+    protected function _getStart($index, $max, $maxHeight, $start = null)
+    {
+        if ($start !== null) {
+            if ($index >= ($start + $maxHeight - 1)) {
+                return  max(
+                    min(
+                        $index - $maxHeight + 2,
+                        $max - $maxHeight + 1
+                    ),
+                    0
+                );
+            }
+
+            if ($index <= $start) {
+                return  max(
+                    min(
+                        $index - 1,
+                        $max - $maxHeight + 1
+                    ),
+                    0
+                );
+            }
+
+            return $start;
+        } else {
+            return  max(
+                min(
+                    $index - floor($maxHeight / 2),
+                    $max - $maxHeight + 1
+                ),
+                0
+            );
+        }
+    }
+
+    protected function _prepareLabel($label, $i)
+    {
+        return $label;
+    }
+
+    protected function _keyEvent($char)
+    {
+        $key = $this->getInput()->getKeys();
+        switch ($char) {
+            case $key::ENTER:
+                return $this->_focused;
+
+            case $key::ESC:
+                return false;
+        }
+
+        return null;
     }
 }
